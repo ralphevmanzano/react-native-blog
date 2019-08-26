eb69bd05d07e171b88b8e1e152e8703846bf1070
ralph <ralphevmanzano@gmail.com>
2019-08-26 14:25:19 +0800

    Initial commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://867c2ef7.ngrok.io'
+});
---
+++ src/screens/EditScreen.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+import BlogPostFrom from '../components/BlogPostForm';
+
+const EditScreen = ({ navigation }) => {
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
+
+  const blogPost = state.find(
+    (blog) => blog.id === id
+  );
+
+  return <BlogPostFrom
+    initialValues={{ title: blogPost.title, content: blogPost.content }}
+    onSubmit={(title, content) => editBlogPost(id, title, content, () => navigation.navigate('Index'))}
+  />;
+}
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
